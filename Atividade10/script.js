ce54a1ea5ad1c7e870c85932e3eed3046559aa36
JoaoVitorRabelo <78799931+JoaoVitorRabelo@users.noreply.github.com>
2022-05-09 12:56:39 -0300

    Add files via upload

---
+++ Atividade10/script.js
@@ -0,0 +1,14 @@
+var aluno = { 
+    name: "Manoela Rodriguez",
+    ra: 1234};
+alert("Nome: " + aluno.name + "\n" + "RA: "+ aluno.ra);    
+
+var aluno2=new Object();
+aluno2.name="Ricardo de Souza";
+aluno2.ra=4321;
+alert("Nome: " + aluno2.name + "\n" + "RA: "+ aluno2.ra);
+
+var aluno3= {};
+aluno3.name="Taylor Rapouso";
+aluno3.ra=4132;
+alert("Nome: " + aluno3.name + "\n" + "RA: "+ aluno3.ra);
